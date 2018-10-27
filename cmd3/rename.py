051201a227301a3df1e3f228c16a06978120d3a0
physics-sp <physics-sp@protonmail.com>
2018-10-27 11:39:39 -0300

    add cmd3 funcionando

--- cmd3/rename.py
+++ cmd3/rename.py
@@ -1,13 +1,9 @@
 def rename(string):
-    matches = re.findall(r'[a-z]+', string)
+    matches = re.findall(r'[a-zA-Z]+', string)
     uniques = []
     for match in matches:
         if match not in uniques:
             uniques.append(match)
-    name = {}
     for i in range(len(uniques)):
-        newname = '_'*(2+i)
-        name[uniques[i]] = newname
-    for i in range(len(uniques)):
-        string = re.sub(uniques[i], name[uniques[i]], string)
+        string = re.sub(uniques[i], '_'*(2+i), string)
     print(string)
